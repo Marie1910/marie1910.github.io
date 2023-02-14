caeb0bf0f644c4b61a40f31dace1015d2151fabe
Salia <yukina.peace@gmail.com>
2023-02-14 11:31:41 +0100

    random color functionality

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 function randomizeLoading() {
   console.log(document.getElementById("basic"));
-  document.getElementById("basic").style.background = "#04AA6D";
+  console.log(getRandomColor());
+  console.log(randomColor(220));
+  document.getElementById("basic").style.background = getRandomColor();
+}
+
+function randomColor(brightness) {
+  function randomChannel(brightness) {
+    var r = 255 - brightness;
+    var n = 0 | (Math.random() * r + brightness);
+    var s = n.toString(16);
+    return s.length == 1 ? "0" + s : s;
+  }
+  return (
+    "#" +
+    randomChannel(brightness) +
+    randomChannel(brightness) +
+    randomChannel(brightness)
+  );
+}
+
+// Function to generate random number
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomColor() {
+  randomizer = Math.random();
+  while (randomizer == 0.0 || randomizer == 0.5) {
+    randomizer = Math.random();
+  }
+  return (
+    "#" +
+    (0x1000000 + randomizer * 0xffffff)
+      .toString(16)
+      .substring(1, 7)
+      .toUpperCase()
+  );
 }
